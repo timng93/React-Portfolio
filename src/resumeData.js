a9d8e5f0a9de264c45422f9e9402e0767035f8e5
Timmy <timgabrielnguyen@gmail.com>
2019-04-29 22:12:44 -0700

    Styled Banner Section

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,99 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  name: "Tim",
+  role: "Full-Stack Developer",
+  linkedinId: "https://www.linkedin.com/in/timng93/",
+  skypeid: "timng93",
+  roleDescription:
+    " I am a values driven developer that is motivated by complex problems and new knowledge of the web. Although I am relatively new to the world of code, my confidence grows daily as I take on increasingly challenging projects. My dedication, hard work, and ability to learn quickly are what make me a great asset to any team.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/timng93/",
+      className: "fa fa-linkedin"
+    },
+    {
+      name: "github",
+      url: "https://github.com/timng93",
+      className: "fa fa-github"
+    }
+  ],
+  aboutme:
+    "I graduated with a Bachelor Degree in Biology from UBC in 2015. I incidentally explored Coding in the summer 2018 and decided to make a career switch as a developer. At RED Academy, I built my portfolio to showcase my works as a Full-Stack Developer",
+  address: "Vancouver Area",
+  education: [
+    {
+      UniversityName: "Harvard University - Distance Education",
+      specialization: "React Native Certificate",
+      MonthOfPassing: "Currently Pursuing"
+    },
+    {
+      UniversityName: "Red Academy - Vancouver BC",
+      specialization: "Full Stack Diploma",
+      MonthOfPassing: "March",
+      YearOfPassing: "2019"
+    },
+    {
+      UniversityName: "Brain Station- Vancouver BC",
+      specialization: "Certificate UX Design",
+      MonthOfPassing: "August",
+      YearOfPassing: "2018"
+    },
+    {
+      UniversityName: "University of British Columbia - Vancouver BC",
+      specialization: "Bachelor of Science in Biology",
+      MonthOfPassing: "April",
+      YearOfPassing: "2015"
+    }
+  ],
+  work: [
+    {
+      CompanyName: "BC Sustainable Energy Association",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements"
+    },
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements"
+    }
+  ],
+  skills: [
+    {
+      skillname: "React"
+    },
+    {
+      skillname: "CSS"
+    },
+    {
+      skillname: "PHP"
+    }
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg"
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg"
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png"
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg"
+    }
+  ]
+};
+
+export default resumeData;
