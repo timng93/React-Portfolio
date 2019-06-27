67e8fdc285577cb7ee0f625e1937e78896e04b0c
Timmy <timgabrielnguyen@gmail.com>
2019-06-27 16:09:09 -0700

    Updates

--- precache-manifest.e1aa6e3de4ea82aa1d48c1d3631eab0c.js
+++ precache-manifest.e1aa6e3de4ea82aa1d48c1d3631eab0c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/tim-portfolio/static/js/runtime~main.73f6cf90.js"
   },
   {
-    "revision": "6181eaa13f8f35ce8498",
-    "url": "/tim-portfolio/static/js/main.6181eaa1.chunk.js"
+    "revision": "85ed0101092d91649fe8",
+    "url": "/tim-portfolio/static/js/main.85ed0101.chunk.js"
   },
   {
     "revision": "e990ccfaedfecdf9af24",
     "url": "/tim-portfolio/static/js/1.e990ccfa.chunk.js"
   },
   {
-    "revision": "6181eaa13f8f35ce8498",
+    "revision": "85ed0101092d91649fe8",
     "url": "/tim-portfolio/static/css/main.c8b3fb81.chunk.css"
   },
   {
-    "revision": "3da02724091ed73822d86ef4c75d4026",
+    "revision": "826e9aef409c569b9b58d7d01d4c50e3",
     "url": "/tim-portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tim-portfolio/precache-manifest.6cd5e20588877144c53d84f54450bc5c.js"
+  "/tim-portfolio/precache-manifest.e1aa6e3de4ea82aa1d48c1d3631eab0c.js"
 );
 
 workbox.clientsClaim();
